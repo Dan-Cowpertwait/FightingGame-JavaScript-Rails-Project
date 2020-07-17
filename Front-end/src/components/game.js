bf40ae35f3321bd8392edc46d802fab39c09646c
Dan-zai <danielcowpertwait@gmail.com>
2020-07-17 12:04:14 +1000

    Mechanics Finished, Building Character CSS

--- Front-end/src/components/game.js
+++ Front-end/src/components/game.js
@@ -12,7 +12,6 @@ const attack = () => {
     //variables
     let attackButton = document.getElementById('attack-button');
     let turnMessage = document.getElementById('turn-message');
-    
     //player turn
     let playerAttack = determineAttack(player.power)
     opponent.health -= playerAttack;
@@ -20,14 +19,25 @@ const attack = () => {
     printToScreen();
     
     attackButton.disabled = true;
+    if (isGameOver(opponent.health)){
+        gameOver("You Won!")
+        //return without argument causes funtions to stop running!
+    }
 
     //opponents turn
     turnMessage.innerText = "Opponent's Turn!"
     setTimeout(() => {
         let opponentAttack = determineAttack(opponent.power)
         player.health -= opponentAttack;
+        document.getElementById('damage-message').innerText = `you took ${opponentAttack} damage!`;
         printToScreen();
+
+        if (isGameOver(player.health)){
+            gameOver("You Lose!")
+
+        }
         attackButton.disabled = false;
+        turnMessage.innerText = "Your Turn!"
     }, 1000)
 }
 
@@ -39,6 +49,18 @@ const printToScreen = () => {
 const determineAttack = (power) => {
     return Math.floor(Math.random() * power);
 
+}
+
+const isGameOver = (health) => {
+    return health <= 0;
+    //returns boolean! (interesting...)
+}
+
+const gameOver = (message) => {
+    let gameMessage = document.getElementById('game-message');
+    gameMessage.innerText = message
+    attackButton.hidden = true;
+    return;
 }
 
 // const gameMessages = () => {
